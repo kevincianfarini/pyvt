496f4da810749753d6841084b0eac0979571bce4
Kevin Cianfarini <kevincianfarini@gmail.com>
2016-12-30 17:55:20 -0500

    timetable unavailable exception testing

--- tests/test_timetable.py
+++ tests/test_timetable.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from unittest.mock import patch
+from api.timetable import TimetableUnavailableException
 
 
 class TestTimetable(TestCase):
@@ -7,4 +8,12 @@ class TestTimetable(TestCase):
 
 
 class TestTimetableException(TestCase):
-    pass
+
+    def setUp(self):
+        self.e = TimetableUnavailableException("Test", 1)
+
+    def test_timetable_unavailable_message(self):
+        self.assertEqual("Test", str(self.e))
+
+    def test_timetable_unavailable_exception_sleep_time(self):
+        self.assertEqual(1, self.e.sleep_time)
