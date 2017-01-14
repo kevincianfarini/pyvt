6ea61e1485ab4e0ad31e6a5e8967c963a27214e4
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-01-14 16:22:02 -0500

    bump version 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='py-vt',
     description='A Python API for the Virginia Tech Timetable of Classes',
-    version='0.0.2',
+    version='0.0.3',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6'
